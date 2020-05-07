16b390d35a334666c8cb5d4da2687aa455d40f15
Poorna <bobachandra@gmail.com>
2020-05-07 08:55:45 +0530

    updated to deploy the heroku

--- public/js/app.js
+++ public/js/app.js
@@ -11,7 +11,8 @@ weatherForm.addEventListener('submit',(e) => {
     messageTwo.textContent = ' '
 
     
-    fetch('http://localhost:3000/weather?address=' +searchValue.value).then((response) => {
+  //fetch('http://localhost:3000/weather?address=' +searchValue.value).then((response) => {
+    fetch('/weather?address=' +searchValue.value).then((response) => {
     response.json().then((data) => {
         if (data.error) {
             messageOne.style.color = 'red'
